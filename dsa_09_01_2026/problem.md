e2a3ca21be8ebf859083a6010db4e3153627edde
berryboylb <phemmynesce4life@gmail.com>
2026-01-09 23:23:21 +0100

    @chore: dsa 09

---
+++ dsa_09_01_2026/problem.md
@@ -0,0 +1,8 @@
+## Problem
+<!-- Short summary -->
+
+## Source
+https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+
+## Tags
+<!-- array, hash-map, etc -->
---
+++ dsa_09_01_2026/solution.go
@@ -0,0 +1,21 @@
+// DSA Daily — 2026-01-09
+// Problem: https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+
+package dsa09012026
+
+func findMin(nums []int) int {
+    high, low := len(nums)-1, 0
+
+	for low < high {
+		mid := (low + high) / 2
+
+		if nums[mid] > nums[high] {
+			low = mid + 1
+		} else {
+			high = mid
+		}
+	}
+	return nums[low]
+}
+
+func main() {}
